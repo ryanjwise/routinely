3d6fcdb12c024b750ee6a87b07cb8e8666125dbc
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-04-04 16:02:57 +1000

    rspec update methods + debugging

--- src/spec/application_spec.rb
+++ src/spec/application_spec.rb
@@ -1,20 +1,21 @@
 require_relative '../lib/application'
+require_relative '../lib/routine'
 
 # Clear arguments
 ARGV.clear
 
 RSpec.describe Menu do
-  subject(:app) do
+  subject(:menu) do
     described_class.new
   end
 
   describe 'Class Testing' do
-    it 'should be an instance of App' do
-      expect(app).to be_a Menu
+    it 'should be an instance of menu' do
+      expect(menu).to be_a Menu
     end
 
     it 'should have an empty routine list' do
-      expect(app.routines).to eq []
+      expect(menu.routines).to eq []
     end
   end
 
@@ -22,15 +23,15 @@ RSpec.describe Menu do
     let(:input) { StringIO.new('1') }
     it 'should get input from the user' do
       $stdin = input
-      expect(app.input_number).to eq(1)
+      expect(menu.input_number).to eq(1)
     end
   end
 
   describe 'Routine CRUD' do
     before
     it 'should be able to add new routine items' do
-      app.add_routine('test routine')
-      expect(app.routines.last).to eq({name: 'test routine', total_time: 0})
+      menu.add_routine('test routine')
+      expect(menu.routines.last[:name]).to eq('test routine')
     end
   end
 end
