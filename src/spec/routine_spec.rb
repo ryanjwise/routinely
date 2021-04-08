06fdff6fda86450c6f9e1201c1ecca9854f7b4e0
Ryan Wise <48581165+ryanjwise@users.noreply.github.com>
2021-04-08 14:35:25 +1000

    Spec, calculate and load methods

--- src/spec/routine_spec.rb
+++ src/spec/routine_spec.rb
@@ -10,4 +10,64 @@ RSpec.describe Routine do
       expect(routine).to be_a Routine
     end
   end
+
+  # [{"name":"Routine1","events":[{"name":"stuff","time":15},{"name":"larger things","time":45},{"name":"shovelling","time":25},{"name":"pancakes","time":15},{"name":"Some more stuff","time":60}],"total_time":160,"start_time":"0000","finish_time":"1:25"}
+
+  describe '#from_JSON' do
+    before(:each) do
+      events = [{name:"event1",time:15}, {name:"event2",time:45}]
+      total_time = 160
+      start_time = "00:00"
+      finish_time = "02:40"
+      routine.from_json(events, total_time, start_time, finish_time)
+    end
+
+    it 'should add events correctly' do
+      expect(routine.inspect).to include("test")
+    end
+
+    it 'should add events correctly' do
+      expect(routine.inspect).to include({:name => "event1", :time => 15}.to_s)
+    end
+
+    it 'should add total time correctly' do
+      expect(routine.inspect).to include(160.to_s)
+    end
+
+    it 'should add start time correctly' do
+      expect(routine.inspect).to include("00:00")
+    end
+
+    it 'should add finish time correctly' do
+      expect(routine.inspect).to include("02:40")
+    end
+  end
+
+  describe 'time calculations' do
+    context 'modify start/end time' do
+      before(:each) do
+        events = [{name:"event1",time:15}, {name:"event2",time:45}]
+        total_time = 160
+        start_time = "00:00"
+        finish_time = "00:00"
+        routine.from_json(events, total_time, start_time, finish_time)
+      end
+
+      it 'should output the correct end time from a default string' do
+        expect(routine.calculate_finish_time).to eq("02:40")
+      end
+
+      it 'should output the correct end time from a specified string' do
+        expect(routine.calculate_finish_time("12:00")).to eq("14:40")
+      end
+
+      it 'should output the correct end time when number of minutes equals +1 hour' do
+        expect(routine.calculate_finish_time("12:30")).to eq("15:10")
+      end
+
+      it 'should output the correct end time when moving past midnight' do
+        expect(routine.calculate_finish_time("23:00")).to eq("01:40")
+      end
+    end
+  end
 end
